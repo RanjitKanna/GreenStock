243a46600017dd6179f7e273bc3f5fbda91a8ef5
Test <Test@gmail.com>
2026-09-05 11:53:14 +0530

    feat: add google-services.json for Firebase configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.green_stocks"
+    namespace = "com.greenstock.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.green_stocks"
+        applicationId = "com.greenstock.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
